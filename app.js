0ceed02e07ee05ef23609cc0fec363f32bbde89e
Travis Ko <tkospeaks@gmail.com>
2021-01-20 20:18:31 -0800

    Add variables for port info, start function and connect function

---
+++ app.js
@@ -0,0 +1,66 @@
+// Import both mySQL and inquirer // 
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const { start } = require("repl");
+
+// Create connection for information on the SQL database //
+const connection = mysql.createConnection({
+    host:"localhost",
+    // Add in port to use
+    port: 9000,
+    // Username and password //
+    user: "root",
+    password: "password",
+    database: "employeesDB",
+
+
+});
+
+// Connect to mysql server and established database //
+connection.connect((err)) => {
+    if (err) {
+        throw err;
+    }
+    return start();
+}
+
+// Create start function to prompt user //
+function start() {
+    return inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "ADD",
+          "VIEW",
+          "UPDATE",
+          "DELETE",
+          "Exit",
+        ],
+      })
+      .then((answer) => {
+        // Using the answer input, either call the bid or the post functions for input
+        switch (answer.action) {
+          case "ADD":
+            addInfo();
+            break;
+          case "VIEW":
+            viewInfo();
+            break;
+          case "UPDATE":
+            updateInfo();
+            break;
+          case "DELETE":
+            deleteInfo();
+            break;
+          default:
+            connection.end();
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        process.exit(1);
+      });
+  }
+
